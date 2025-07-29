9cf53c40554651cb984570ce8d92a20bc75e0f44
Askar Gillani <gillani.askar@gmail.com>
2025-07-29 23:28:11 +0500

    Remove uniqueness constraint from email field in user schema

--- api/models/domain/user.js
+++ api/models/domain/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   messagesLeft: { type: Number, default: 20000 },
   name: { type: String, required: true, default: '' },
-  email: {type: String, unique: true}
+  email: {type: String}
 });
 
 module.exports = mongoose.model('User', userSchema);
